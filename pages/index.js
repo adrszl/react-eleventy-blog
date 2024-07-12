1c4c97cbb3cda7e50037bcb423295cc891eec97e
Adrian Szlegel <adrian.szlegel@hotmail.com>
2024-07-12 23:01:07 +0200

    first post added

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from "next/Link";
 
 export default function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <Image src="/../public/logo.svg" className="App-logo" alt="logo" width={300} height={300} />
-        <span>Adrian Szlegel</span>
+        <span>Adrian Szlegel - blog based on NextJS</span>
         <p>Another cool project will be here 😊</p>
       </header>
+      <Link href="/posts/hello">Hello post</Link>
     </div>
   )
 }
